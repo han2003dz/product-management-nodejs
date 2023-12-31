0c6ced525c81a877c57b8880b736a13b62258499
han2003dz <han20031605@gmail.com>
2023-12-31 22:24:49 +0700

    tính năng tìm kiếm

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,5 +1,27 @@
-module.exports.index = (req, res) => {
+const filterStatusHelper = require("../../helpers/filterStatus");
+const searchHelper = require("../../helpers/search");
+const Product = require("../../models/products.model");
+
+module.exports.index = async (req, res) => {
+  const filterStatus = filterStatusHelper(req.query);
+  const objectSearch = searchHelper(req.query);
+
+  let find = {
+    deleted: false,
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  if (objectSearch.regex) {
+    find.title = objectSearch.regex;
+  }
+
+  const products = await Product.find(find).limit(4);
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh sách sản phẩm",
+    products,
+    filterStatus,
+    keyword: objectSearch.keyword,
   });
 };
