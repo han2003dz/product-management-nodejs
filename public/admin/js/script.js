4c024c5eefc76cfb2419874853035206be3d18a3
han2003dz <han20031605@gmail.com>
2024-01-20 16:17:41 +0700

    sự kiện checkbox-multi

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -122,3 +122,33 @@ if (uploadImage) {
   console.log(uploadImagePreview.src);
 }
 // end upload image
+
+// checkbox-multi
+const checkboxMulti = document.querySelector("[checkbox-multi]");
+if (checkboxMulti) {
+  const checkAll = checkboxMulti.querySelector("input[name='checkAll']");
+  const checkItem = checkboxMulti.querySelectorAll("input[name='check-item']");
+  checkAll.addEventListener("click", () => {
+    if (checkAll.checked) {
+      checkItem.forEach((item) => {
+        item.checked = true;
+      });
+    } else {
+      checkItem.forEach((item) => {
+        item.checked = false;
+      });
+    }
+  });
+  checkItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      const countChecked = checkboxMulti.querySelectorAll(
+        "input[name='check-item']:checked"
+      ).length;
+      if (countChecked == checkItem.length) {
+        checkAll.checked = true;
+      } else {
+        checkAll.checked = false;
+      }
+    });
+  });
+}
