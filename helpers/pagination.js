4f14e612e4b0d31bf82acbc8543086f0f32967d4
han2003dz <han20031605@gmail.com>
2024-01-31 23:49:58 +0700

    _

--- helpers/pagination.js
+++ helpers/pagination.js
@@ -1,10 +1,12 @@
 module.exports = (objectPagination, query, totalRecord) => {
   objectPagination.currentPage = parseInt(query.page) || 1; // Sử dụng optional chaining và nullish coalescing để xử lý query.page không được định nghĩa
   objectPagination.currentPage = Math.max(1, objectPagination.currentPage); // Đảm bảo trang hiện tại không âm
-  const totalPages = Math.ceil(totalRecord / objectPagination.limitItem);
-  objectPagination.totalPages = totalPages;
   objectPagination.skip =
     (objectPagination.currentPage - 1) * objectPagination.limitItem;
   objectPagination.skip = Math.min(objectPagination.skip, totalRecord); // Đảm bảo skip không vượt quá tổng số bản ghi
+
+  const totalPages = Math.ceil(totalRecord / objectPagination.limitItem);
+  objectPagination.totalPages = totalPages;
   return objectPagination;
 };
+
