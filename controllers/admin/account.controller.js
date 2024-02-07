81c59febe62af7dcf16c2c4f3160d8f532a6d439
han2003dz <han20031605@gmail.com>
2024-02-07 15:34:59 +0700

    Thêm mới tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,22 +1,61 @@
+const md5 = require("md5");
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
 
 module.exports.index = async (req, res) => {
-  let find = {
-    deleted: false,
-  };
-  const accounts = await Account.find(find);
-  res.render("admin/pages/accounts/index.pug", {
-    pageTitle: "Quản lý tài khoản",
-    accounts,
-  });
+  try {
+    let find = {
+      deleted: false,
+    };
+    const accounts = await Account.find(find).select("-password -token");
+    for (const account of accounts) {
+      const role = await Role.findOne({
+        _id: account.role_id,
+        deleted: false,
+      });
+      account.role = role;
+    }
+    res.render("admin/pages/accounts/index.pug", {
+      pageTitle: "Quản lý tài khoản",
+      accounts,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports.create = async (req, res) => {
-  res.render("admin/pages/accounts/create", {
-    pageTitle: "Thêm tài khoản mới",
-  });
+  try {
+    const roles = await Role.find({ deleted: false });
+    res.render("admin/pages/accounts/create", {
+      pageTitle: "Thêm tài khoản mới",
+      roles,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
-
 module.exports.createPost = async (req, res) => {
-  res.send("OK");
+  try {
+    const emailExist = await Account.findOne({
+      email: req.body.email,
+      deleted: false,
+    });
+
+    if (emailExist) {
+      req.flash("error", `Email ${req.body.email} đã tồn tại`);
+      res.redirect("back");
+    } else {
+      req.body.password = md5(req.body.password);
+      const account = new Account(req.body);
+      await account.save();
+      res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+      req.flash("success", "Tạo thành công tài khoản mới");
+    }
+  } catch (error) {
+    req.flash("error", "Chưa tạo được tài khoản mới");
+    res.redirect("back");
+    console.log(error);
+  }
 };
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const router = express.Router();
+
+const multer = require("multer");
+const upload = multer();
+const uploadCloud = require("../../middlewares/admin/uploadCloudinary.middleware");
+
 const controller = require("../../controllers/admin/account.controller");
+
 router.get("/", controller.index);
 router.get("/create", controller.create);
+router.post(
+  "/create",
+  upload.single("avatar"),
+  uploadCloud.upload,
+  controller.createPost
+);
 module.exports = router;
